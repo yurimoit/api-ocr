202b8a937b6c014c40dff7f09ebc99793bbffbec
yurimoura <yurimoura9626@outlook.com>
2023-10-29 19:35:13 -0300

    teste2

--- server.py
+++ server.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pyodbc
 
 app=Flask(__name__)
 
+server = 'leitorappocr.database.windows.net'
+database = 'leitor-ocr'
+username = 'azurer'
+password = 'Destroi96&'
+port=1433
+
+dadosBanco = (
+               'Driver={ODBC Driver 18 for SQL Server};'
+               f'Server={server};'
+               f'Database={database};'
+               f'Uid={username};'
+               f'Pwd={password};'
+               f'Port={port};'
+)   
+
 @app.route("/")
 def helloWorld():
-    return f"<p>Hello, world!<p>"
+    try:
+        conexao = pyodbc.connect(dadosBanco)
+        cursor = conexao.cursor()
+        cursor.execute("select * from usuarios")
+        resultado = cursor.fetchall()
+        conexao.close()
+        
+        data_list = [dict(zip([column[0] for column in cursor.description], row)) for row in resultado]
+
+        return jsonify({'resultado': data_list})  
+    except pyodbc.Error as e: 
+        return jsonify({'erro': str(e)})
 
 if __name__ == "__main__":
-   app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)
