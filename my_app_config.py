ed75774ec80dd37c93adacaff6f358ece19fd2b9
yurimoura <yurimoura9626@outlook.com>
2024-04-24 19:59:52 -0300

    teste de correção

--- my_app_config.py
+++
@@ -1,5 +0,0 @@
-# my_app_config.py
-
-bind = '0.0.0.0:1433'  # Endereço e porta para o Gunicorn ouvir
-workers = 4  # Número de processos de trabalho (ajuste conforme necessário)
-timeout = 60  # Tempo limite para solicitações
