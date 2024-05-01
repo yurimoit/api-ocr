3b30ceaacdccfdac5302c54f516ab96cde52adf2
yurimoura <yurimoura9626@outlook.com>
2024-04-30 23:05:08 -0300

    add da api ocr do google 23

--- controladorArquivo/controlador_arquivos.py
+++ controladorArquivo/controlador_arquivos.py
@@ -41,7 +41,7 @@ def detect_text(image):
         "type": TYPE_VALUE,
         "project_id": PROJECT_ID,
         "private_key_id": PRIVATE_KEY_ID,
-        "private_key": str(PRIVATE_KEY),
+        "private_key": PRIVATE_KEY.replace("\\n", "\n"),
         "client_email": CLIENT_EMAIL,
         "client_id": CLIENT_ID,
         "auth_uri": AUTH_URI,
