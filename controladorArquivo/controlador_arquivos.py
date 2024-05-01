88fb76ab928d13f39631946985e6b49633c7a527
yurimoura <yurimoura9626@outlook.com>
2024-04-30 22:08:42 -0300

    add da api ocr do google 17

--- controladorArquivo/controlador_arquivos.py
+++ controladorArquivo/controlador_arquivos.py
@@ -38,17 +38,17 @@ def detect_text(image):
           AUTH_URI, TOKEN_URI, AUTH_PROVIDER_X509_CERT_URL, CLIENT_X_CERT_URL, UNIVERSE_DOMAIN)
 
     credentials_json = {
-        "type": str(TYPE_VALUE),
-        "project_id": str(PROJECT_ID),
-        "private_key_id": str(PRIVATE_KEY_ID),
-        "private_key": str(PRIVATE_KEY),
-        "client_email": str(CLIENT_EMAIL),
-        "client_id": str(CLIENT_ID),
-        "auth_uri": str(AUTH_URI),
-        "token_uri": str(TOKEN_URI),
-        "auth_provider_x509_cert_url": str(AUTH_PROVIDER_X509_CERT_URL),
-        "client_x509_cert_url": str(CLIENT_X_CERT_URL),
-        "universe_domain": str(UNIVERSE_DOMAIN)
+        "type": TYPE_VALUE,
+        "project_id": PROJECT_ID,
+        "private_key_id": PRIVATE_KEY_ID,
+        "private_key": PRIVATE_KEY,
+        "client_email": CLIENT_EMAIL,
+        "client_id": CLIENT_ID,
+        "auth_uri": AUTH_URI,
+        "token_uri": TOKEN_URI,
+        "auth_provider_x509_cert_url": AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": CLIENT_X_CERT_URL,
+        "universe_domain": UNIVERSE_DOMAIN
     }
 
     print("CREEE", credentials_json)
